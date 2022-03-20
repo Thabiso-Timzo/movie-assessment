b86115bb66670411cedd33fccbd0c312217ebca5
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-03-20 22:19:18 +0200

    Add the user router model and auth file, install some of the packages

--- config/db.js
+++ config/db.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose');
 module.exports = async () => {
     try {
         const connectionParams = { useNewUrlParser: true, useUnifiedTopology: true }
-        await mongoose.connect('mongodb+srv://Gudee:Addmore%40Digital@cluster0.tisxh.mongodb.net/Gudee?retryWrites=true&w=majority', connectionParams) {
-            console.log("Database connected!");
-        } 
+        await mongoose.connect('mongodb+srv://movieProject:DbdFaqxwdLkmN6Gb@cluster0.u7o09.mongodb.net/MovieProject?retryWrites=true&w=majority', connectionParams);
+        console.log("Database connected!");
+         
     } catch (error) {
         console.log(error);
         console.log('Could not connect to the database');
     }
 }
+
+//DbdFaqxwdLkmN6Gb
