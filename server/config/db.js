0c952a45b5bdd0d93e1af71029140418b21e120f
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-10 17:56:13 +0200

    Modified db.js file

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const MongoDBConnect = async () => {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         })
-        console.log(`MongoDB connected: ${conn.connection.host}`)
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.bold)
     } catch (error) {
         console.error(`Error: ${error.message}`)
         process.exit()
