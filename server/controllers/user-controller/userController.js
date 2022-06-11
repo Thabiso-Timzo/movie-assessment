e42dd3d817b8bcb5bbe5d02408fc21fa16f81631
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-11 14:24:17 +0200

    Added user controller

---
+++ server/controllers/user-controller/userController.js
@@ -0,0 +1,103 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asycHandler = require('express-async-handler');
+
+const User = require('../../models/user-schema/UserSchema');
+
+/**
+ * @desc Register new user
+ * @route POST /api/users/
+ * @access Public
+ */
+const registerUser = asycHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields');
+    }
+
+    // Check if the user exists
+    const userExists = await User.findOne({email});
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists');
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data');
+    }
+})
+
+/**
+ * @desc Login a user
+ * @route POST /api/users/login
+ * @access Public
+ */
+const loginUser = asycHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // check for user password
+    const user = await User.findOne({email});
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials');
+    }
+})
+
+/**
+ * @desc GET user data
+ * @route GET /api/users/me
+ * @access Private
+ */
+const getMe = asycHandler(async (req, res) => {
+    const { _id, name, email } = await User.findById(req.user.id);
+
+    res.status(200).json({
+        id: _id,
+        name,
+        email,
+    })
+})
+
+// Generate token 
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+
+    })
+}
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe,
+}
