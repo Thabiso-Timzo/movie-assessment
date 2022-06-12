6f06feb9d21356f341417d44070d4cc44633e7fc
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-12 16:21:18 +0200

    Updated Login functionality

--- client/src/authentication/login/Login.jsx
+++ client/src/authentication/login/Login.jsx
@@ -1,12 +1,69 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { FaFacebookSquare } from "react-icons/fa"
 import { FcGoogle } from "react-icons/fc";
+import { useSelector, useDispatch } from 'react-redux'
+import { toast } from 'react-toastify'
  
 import './Login.css'
 import image1 from '../../images/1.jpg'
+import { login, reset } from '../../redux/auth/authSlice'
+import Spinner from '../../components/spinner/Spinner'
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { 
+        user, 
+        isLoading, 
+        isError, 
+        isSuccess, 
+        message 
+    } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        if (isSuccess || user) {
+            navigate('/movies');
+        }
+
+        dispatch(reset());
+
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const userData = {
+            email,
+            password
+        }
+
+        dispatch(login(userData));
+    }
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
   return (
     <section>
         <div className="image-box">
@@ -15,14 +72,28 @@ const Login = () => {
         <div className="content-box">
             <div className="form-box">
                 <h2>Login</h2>
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className="input-box">
-                        <span>Username</span>
-                        <input type="text" />
+                        <span>Email</span>
+                        <input 
+                            type="text"
+                            id="email" 
+                            name="email" 
+                            value={email} 
+                            placeholder="Enter your email address" 
+                            onChange={onChange} 
+                        />
                     </div>
                     <div className="input-box">
                         <span>Password</span>
-                        <input type="password" />
+                        <input 
+                            type="password"
+                            id="password" 
+                            name="password" 
+                            value={password} 
+                            placeholder="Enter your password" 
+                            onChange={onChange} 
+                        />
                     </div>
                     <div className="remember">
                         <label htmlFor="">
@@ -33,7 +104,7 @@ const Login = () => {
                         <button>Login</button>
                     </div>
                     <div className="input-box">
-                        <p>Don't have an account? <Link to='register'>Register</Link></p>
+                        <p>Don't have an account? <Link to='/register'>Register</Link></p>
                     </div>
                 </form>
                 <h3>Or Login with...</h3>
