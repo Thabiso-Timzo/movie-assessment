97cc17d31aafa94dd3e28a259658d4f067bbf14c
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-12 16:22:43 +0200

    Updated register functionality

--- client/src/authentication/register/Register.jsx
+++ client/src/authentication/register/Register.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { toast } from 'react-toastify'
 
 import './Register.css'
 import image2 from '../../images/2.jpg'
+import { register, reset } from '../../redux/auth/authSlice'
+import Spinner from '../../components/spinner/Spinner'
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { 
+        user, 
+        isLoading, 
+        isError, 
+        isSuccess, 
+        message 
+    } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message);
+        }
+
+        if (isSuccess || user) {
+            navigate('/login');
+        }
+
+        dispatch(reset());
+
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (password !== password2) {
+            toast.error('Passwords do not match.')
+        } else {
+            const userData = {
+                name, email, password
+            }
+
+            dispatch(register(userData))
+        }
+    }
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
   return (
     <section>
         <div className="image-box">
@@ -13,22 +74,50 @@ const Register = () => {
         <div className="content-box">
             <div className="form-box">
                 <h2>Register</h2>
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className="input-box">
-                        <span>Username</span>
-                        <input type="text" />
+                        <span>Full name</span>
+                        <input 
+                            type="text" 
+                            id="name" 
+                            name="name" 
+                            value={name} 
+                            placeholder="Enter your user full name" 
+                            onChange={onChange}
+                        />
                     </div>
                     <div className="input-box">
                         <span>Email</span>
-                        <input type="text" />
+                        <input 
+                            type="text"
+                            id="email" 
+                            name="email" 
+                            value={email} 
+                            placeholder="Enter your email" 
+                            onChange={onChange}
+                        />
                     </div>
                     <div className="input-box">
                         <span>Password</span>
-                        <input type="password" />
+                        <input 
+                            type="password"
+                            id="password" 
+                            name="password" 
+                            value={password} 
+                            placeholder="Enter your password" 
+                            onChange={onChange} 
+                        />
                     </div>
                     <div className="input-box">
-                        <span>Re-enter Password</span>
-                        <input type="password" />
+                        <span>Confirm Password</span>
+                        <input 
+                            type="password" 
+                            id="password2" 
+                            name="password2" 
+                            value={password2} 
+                            placeholder="Confirm your password" 
+                            onChange={onChange}
+                        />
                     </div>
                     <div className="input-box">
                         <button>Register</button>
