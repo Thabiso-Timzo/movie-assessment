40c51742230984e3ae4bb5aea566d1144d2a99b8
Thabiso-Timzo <mr.tt.hlatshwayo@gmail.com>
2022-06-15 12:31:46 +0200

    Modified home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import './Home.css'
 
 function Home() {
-  //const API_KEY = d4f7b87d7cedfdfbbb297f46aa3e8779
+  const { user } = useSelector((state) => state.auth)
 
   return (
     <div className="home-container">
         <h2>Welcome to Fantastic Films</h2>
         <br />
-        <Link to="/movies" className="btn-container">
-          <button className="movie-btn">Browse films</button>
-        </Link> 
+        {user ? (
+          <Link to="/movies" className="btn-container">
+            <button className="movie-btn">Browse films</button>
+          </Link>
+        ) : (
+          <Link to="/login" className="btn-container">
+            <button className="movie-btn">Go to login page</button>
+          </Link>
+        )}
+         
     </div>
   )
 }
